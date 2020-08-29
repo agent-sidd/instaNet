a7a5d6d49c8cf110d407a4dd7da333861aa4de6d
Sidddharth singh <60338719+agent-sidd@users.noreply.github.com>
2020-08-29 17:51:13 +0530

    Add files via upload

---
+++ assets/dist/js/script.js
@@ -0,0 +1,68 @@
+
+const _ = e => document.querySelector(e);
+const render = _('.result');
+/*  creating video*/
+const createVideo=data=>{
+let v=document.createElement('video');
+v.id="instavideo";
+v.src=data.content;
+v.controls=true;
+v.autoplay=true;
+
+//info part
+let info=document.createElement('h4');
+info.textContent="right click on video or download button to save the video localy &#128525";
+render.innerHTML="";
+render.appendChild(v);
+render.appendChild(info);
+};
+//
+const createImg= data =>{
+    let br=document.createElement('br');
+    
+    let i=document.createElement('img');
+    i.id="instaImg";
+    i.src=data.content;
+    //info part
+    let info=document.createElement('h4');
+    info.textContent="right click on image or download button to save the image localy";
+    render.innerHTML="";
+    render.appendChild(i);
+    render.appendChild(br);
+    render.appendChild(info);
+    }
+    //extraction
+    const getCont= () =>{
+        render.innerHTML = "<div class='contnt-placeholder'></div>";
+       // fetch url
+       let url =_('input').value;
+       if(url){
+           fetch(url).
+           then(r => r.text()).
+           then(r => {
+             render.innerHTML=r;
+               //extracting meta
+               let wt=setTimeout(()=>{
+                let v = _('meta[property="og:video"]');
+                if(v){
+                   createVideo(v);
+                 }
+                 else{
+                     let img=_('meta[property="og:image"]');
+                     if(img){
+                         createImg(img);
+                     }
+                     else{
+                        document.body.innerHTML= body ;
+                         alert('alert in extracting media.');
+                     };
+                  }
+                  clearTimeout(wt);
+               },200);
+           });
+        }
+           else{
+            _('input').setAttribute('placeholder', 'Invalid address, use a proper Insagram link');
+        
+       }
+    };
